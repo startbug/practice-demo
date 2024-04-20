c0646257df952c75bac40871b6fdfaab522dab99
lianghaohui <1285226024@qq.com>
2024-04-20 12:29:40 +0800

    feat:修改java文件

--- spring-event-pratice/src/main/java/com/ggs/event/controller/FileController.java
+++ spring-event-pratice/src/main/java/com/ggs/event/controller/FileController.java
@@ -36,7 +36,7 @@ public class FileController {
     @GetMapping("/add")
     public R<Boolean> addFile() {
         File file = new File();
-        file.setType(".jpg");
+        file.setType(".png");
         file.setSize(3333L);
         file.setName("测试1111zjzz");
         file.setUserId(1L);
